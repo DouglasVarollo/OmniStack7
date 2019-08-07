2291534985a24226a90de9bf976220541ee9bb95
Douglas Varollo <dcvarollo@gmail.com>
2019-08-07 17:37:52 -0300

    back-end finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb://username:p455w0rd@ds335957.mlab.com:35957/omnistack7', {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3333); 
